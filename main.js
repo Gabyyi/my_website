936ec0ba9f4de660c8309ff4251be070bf34fdc0
Gabyyi <90030416+Gabyyi@users.noreply.github.com>
2024-01-29 17:55:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed=new Typed(".text", {
+    strings:["Hello World...","I am Gabriel Pruteanu"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+/*
+var typed=new Typed(".text2", {
+    strings:["I am Gabriel Pruteanu"],
+    typeSpeed:10000,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});*/
